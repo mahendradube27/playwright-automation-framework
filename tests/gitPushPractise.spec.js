737aab2a5c6145f4538c3494e7badd1703ec7b0a
Mahendra Dube <mahendra.dube27@gmail.com>
2026-02-10 11:28:56 +0530

    Updated Changes as Per suggestion in PR Review

--- tests/gitPushPractise.spec.js
+++ tests/gitPushPractise.spec.js
@@ -1,14 +1,15 @@
 import {test} from "@playwright/test"
 
-test('Git Push Practise' , async()=>{
-    console.log('Git Push Practise Test')
-})
-
 test('Git Push Practise 1' , async()=>{
-    console.log('Git Push Practise Test 2')
+    console.log('Git Push Practise Test 1')
+    console.log('Git Push Practise Test 1')
 })
 
 test('Git Push Practise 2' , async()=>{
     console.log('Git Push Practise Test 2')
+})
+
+test('Git Push Practise 3' , async()=>{
+    console.log('Git Push Practise Test 3')
 })
 
