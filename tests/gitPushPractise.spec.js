4c25673b8055c11521725d422d8a7f1c1e9c0329
Mahendra Dube <mahendra.dube27@gmail.com>
2026-02-10 10:57:02 +0530

    Changes Done in File gitPushPractise, added 3 Tests

---
+++ tests/gitPushPractise.spec.js
@@ -0,0 +1,14 @@
+import {test} from "@playwright/test"
+
+test('Git Push Practise' , async()=>{
+    console.log('Git Push Practise Test')
+})
+
+test('Git Push Practise 1' , async()=>{
+    console.log('Git Push Practise Test 2')
+})
+
+test('Git Push Practise 2' , async()=>{
+    console.log('Git Push Practise Test 2')
+})
+
