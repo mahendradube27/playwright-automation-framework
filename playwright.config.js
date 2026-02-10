b2a17987b75837dd45ff747fa9a7ea48ee3fe3f8
Mahendra Dube <mahendra.dube27@gmail.com>
2026-02-10 15:50:51 +0530

    Added Allure Report

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import AllureReporter from 'allure-playwright';
 import dotenv from "dotenv"
 import path from "path"
 
@@ -27,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['allure-playwright' , {outputFolder : 'allure-results'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
