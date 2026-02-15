5e54f002df2a50c16c0a3967cb8dd9e954829aca
Mahendra Dube <mahendra.dube27@gmail.com>
2026-02-15 11:02:01 +0530

    Making changes to Test Jenkins

--- playwright.config.js
+++ playwright.config.js
@@ -28,17 +28,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], /*['github'] , ['allure-playwright' , {outputFolder : 'allure-results'}]*/],
+  reporter: [['html'], ['allure-playwright']/*['github'] , ['allure-playwright' , {outputFolder : 'allure-results'}]*/],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on',
     headless : true,
-    screenshot : 'only-on-failure',
-    video : 'retain-on-failure',
+    screenshot : 'on',
+    video : 'on',
   },
 
   /* Configure projects for major browsers */
@@ -48,15 +48,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
